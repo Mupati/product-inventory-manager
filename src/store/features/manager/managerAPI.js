54e6262a94cca70a12a7ceb0d23261d8a5dd2939
Mupati <scientificgh@gmail.com>
2021-05-28 14:07:15 +0000

    Set mock endpoint to https

--- src/store/features/manager/managerAPI.js
+++ src/store/features/manager/managerAPI.js
@@ -1,4 +1,4 @@
 import axios from "axios";
 
 export const fetchProductData = async () =>
-  await axios.get("http://www.mocky.io/v2/5c3e15e63500006e003e9795");
+  await axios.get("https://www.mocky.io/v2/5c3e15e63500006e003e9795");
