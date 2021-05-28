5bdf039ce7065223d53312f5ab50b79cb76f1214
Mupati <scientificgh@gmail.com>
2021-05-28 15:33:47 +0000

    Add redux-persist to cache state in browser

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { store } from "./store";
+import { store, persistor } from "./store";
 import { Provider } from "react-redux";
 import * as serviceWorker from "./serviceWorker";
 import { CssBaseline } from "@material-ui/core";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <CssBaseline />
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <CssBaseline />
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import managerReducer from "./features/manager/managerSlice";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, managerReducer);
 
 export const store = configureStore({
   reducer: {
-    productManager: managerReducer,
+    productManager: persistedReducer,
   },
 });
+
+export const persistor = persistStore(store);
